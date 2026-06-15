815d1e84b085805d63d92c55bb61d00b40140740
quchenyang <quchenyang@kuaishou.com>
2026-06-15 20:45:45 +0800

    flux返回

--- cowork-webservice-api/src/main/java/com/quchenyang/coworkwebservice/api/controller/EchoController.java
+++ cowork-webservice-api/src/main/java/com/quchenyang/coworkwebservice/api/controller/EchoController.java
@@ -1,5 +1,6 @@
 package com.quchenyang.coworkwebservice.api.controller;
 
+import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -18,7 +20,15 @@ public class EchoController {
     @GetMapping("/echo")
     public Mono<String> echo() {
         log.info("打印一下当前时间");
-        return Mono.just(OffsetDateTime.now(java.time.ZoneOffset.UTC)
+        return Mono.just(OffsetDateTime.now()
                 .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
+    }
+
+    @GetMapping("/echo2")
+    public Flux<String> echo2() {
+        log.info("开始每秒推送当前时间");
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(tick -> String.format("第%d次: %s", tick + 1,
+                        OffsetDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)));
     }
 }
